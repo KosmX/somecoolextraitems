90cfe0f5aa044911c61e94258559ff35462f6457
KosmX <kosmx.mc@gmail.com>
2020-03-22 17:53:08 +0100

    pillows (not ready yet)

--- src/main/java/com/kosmx/somecoolextraitems/blocks/FeatherBlock.java
+++ src/main/java/com/kosmx/somecoolextraitems/blocks/FeatherBlock.java
@@ -1,11 +1,19 @@
 package com.kosmx.somecoolextraitems.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityContext;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
 public class FeatherBlock extends Block {
+    protected static final VoxelShape shape = Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D);
     public FeatherBlock(Settings settings) {
         super(settings);
     }
@@ -13,4 +21,11 @@ public class FeatherBlock extends Block {
     public void onLandedUpon(World world, BlockPos pos, Entity entity, float distance) {
         entity.handleFallDamage(distance/4, 1f);
     }
+
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos) {
+        return facing == Direction.DOWN && !this.canPlaceAt(state, world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, facing, neighborState, world, pos, neighborPos);
+    }
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext context) {
+        return shape;
+     }
 }
