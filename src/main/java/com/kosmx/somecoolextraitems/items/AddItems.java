86d570c69f7ec0cb79868bef0e5594a8277f8df3
KosmX <kosmx.mc@gmail.com>
2020-03-24 16:20:50 +0100

    working on cocoa bucket

--- src/main/java/com/kosmx/somecoolextraitems/items/AddItems.java
+++ src/main/java/com/kosmx/somecoolextraitems/items/AddItems.java
@@ -17,7 +17,7 @@ import net.minecraft.util.registry.Registry;
 public class AddItems {
     public static final Item NetherGoldIngot = new Item(new Item.Settings().group(ItemGroup.MISC));
     public static final Item NetherGoldNugget = new Item(new Item.Settings().group(ItemGroup.MISC));
-    public static final Item DiamondIngot = new DiamondIngotItem(new Item.Settings());
+    //public static final Item DiamondIngot = new DiamondIngotItem(new Item.Settings());
     public static final Item NGoldPickaxe = new NetherGoldPickaxe(new ToolMaterialNethergold());
     public static final Item NGoldAxe = new NetherGoldAxe(new ToolMaterialNethergold());
     public static final Item NGoldSword = new NetherGoldSword(new ToolMaterialNethergold());
@@ -29,6 +29,8 @@ public class AddItems {
     //foods
 
     //TODO peach ... virsli
+    //pillow model in hand(copy normal block)
+    //pillow(every color)
 
     public static final Item Nutella = new NutellaItem(new Item.Settings().food(new FoodComponent.Builder().hunger(6).saturationModifier(6).alwaysEdible().build()).recipeRemainder(Items.GLASS_BOTTLE).group(ItemGroup.FOOD));
     public static final Item BreadWithNutella = new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(6).saturationModifier(6).snack().build()).group(ItemGroup.FOOD));
@@ -56,7 +58,7 @@ public class AddItems {
 
         Registry.register(Registry.ITEM, new Identifier("somecoolextraitems", "nethergold_ingot"), NetherGoldIngot);
         Registry.register(Registry.ITEM, new Identifier("somecoolextraitems", "nethergold_nugget"), NetherGoldNugget);
-        Registry.register(Registry.ITEM, new Identifier("somecoolextraitems", "diamond_ingot"), DiamondIngot);
+        //Registry.register(Registry.ITEM, new Identifier("somecoolextraitems", "diamond_ingot"), DiamondIngot);
 
         Registry.register(Registry.ITEM, new Identifier("somecoolextraitems", "nethergolden_pickaxe"), NGoldPickaxe);
         Registry.register(Registry.ITEM, new Identifier("somecoolextraitems", "nethergolden_axe"), NGoldAxe);
